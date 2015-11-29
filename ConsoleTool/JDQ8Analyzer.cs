1e9c48f3e8aff9d3395f8ba2acc9066a27961988
dayang <1306693844@qq.com>
2015-11-29 15:19:39 +0800

    初步完成8路解析

--- ConsoleTool/JDQ8Analyzer.cs
+++ ConsoleTool/JDQ8Analyzer.cs
@@ -8,9 +8,22 @@ namespace SerialportDataAnalyzer
 {
 	static class JDQ8Analyzer
 	{
-		public static bool Analy(DateTime time, List<KeyValuePair<byte, bool>> messgeQueue)
+        private const string returnStr = "020F00000008543E";
+		public static bool Analy(DateTime time, List<KeyValuePair<byte, bool>> messageQueue)
 		{
-			return true;
+            //!!!!!
+            byte[] bytes = messageQueue.Select(b => b.Key).ToArray();
+
+            String message = Transfer.BaToS(bytes);
+            int index = message.IndexOf(returnStr);
+
+            if (index == -1)
+                return false;
+
+            for (int i = index; i < index + returnStr.Length * 2; i++)
+                messageQueue[i] = new KeyValuePair<byte, bool>(messageQueue[i].Key,false);
+
+            return true;
 		}
 	}
 }
