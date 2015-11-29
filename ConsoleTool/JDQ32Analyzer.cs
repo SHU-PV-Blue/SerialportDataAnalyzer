e4a9273834ce80e5dd965ce813c2f6a6efcdb1a2
Mrwang <1573370091@qq.com>
2015-11-29 16:29:12 +0800

    参照dayang,有dayangQAQ

--- ConsoleTool/JDQ32Analyzer.cs
+++ ConsoleTool/JDQ32Analyzer.cs
@@ -8,9 +8,19 @@ namespace SerialportDataAnalyzer
 {
 	static class JDQ32Analyzer
 	{
-		public static bool Analy(DateTime time, List<KeyValuePair<byte, bool>> messgeQueue)
+		private const String retStr = "010F000000205413";
+		public static bool Analy(DateTime time, List<KeyValuePair<byte, bool>> messageQueue)
 		{
-			return true;
+			byte[] bytes = messageQueue.Select(b => b.Key).ToArray();
+			String message = Transfer.BaToS(bytes);
+			int index = -1;
+			while((index = message.IndexOf(retStr, index + 1)) != -1)
+            {
+                for (int i = index / 2; i < (index + retStr.Length) / 2; i++)
+                    messageQueue[i] = new KeyValuePair<byte, bool>(messageQueue[i].Key, false);
+            }
+				return true;
 		}
+		
 	}
 }
