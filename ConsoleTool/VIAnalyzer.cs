9058d3b56727e1a2f0697f79d4be8e1dbb24b38b
wolfogre <wolfogre@qq.com>
2015-12-03 23:48:40 +0800

    VI解析可以正常工作了

--- ConsoleTool/VIAnalyzer.cs
+++ ConsoleTool/VIAnalyzer.cs
@@ -35,7 +35,8 @@ namespace SerialportDataAnalyzer
                 double Pm = Convert.ToInt64(Inverse(reasultStr.Substring(26, 8)), 16) / 10.0;
 
                 int index = -1;
-                while ((index = byteStr.IndexOf(byteMatch.Value, index + 1)) != -1)
+				//不用while，方朝增本来也这么做，也死循环；每次解析一条就好了
+                if ((index = byteStr.IndexOf(byteMatch.Value, index + 1)) != -1)
                 {
                     for (int i = index / 2; i < (index + byteMatch.Length) / 2; i++)
                         messageQueue[i] = new KeyValuePair<byte, bool>(messageQueue[i].Key, false);
@@ -63,8 +64,8 @@ namespace SerialportDataAnalyzer
                 IVdataDic.Add("MaxPower", Pm.ToString());
                 IVdataDic.Add("Azimuth", azimuth.ToString());
                 IVdataDic.Add("Obliquity", obliquity.ToString());
-                IVdataDic.Add("CurrentSeq", byteMatch.Groups[1].Value);
-                IVdataDic.Add("VoltageSeq", byteMatch.Groups[2].Value);
+				IVdataDic.Add("CurrentSeq", "'" + byteMatch.Groups[1].Value + "'");
+				IVdataDic.Add("VoltageSeq", "'" + byteMatch.Groups[2].Value + "'");
 
                 try
                 {
