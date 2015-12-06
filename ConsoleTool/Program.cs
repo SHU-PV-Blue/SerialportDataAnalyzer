b3087b5face5abfc5ccd5273629f099d65b4e1c1
wolfogre <wolfogre@qq.com>
2015-12-06 10:55:40 +0800

    完善解析器主程序

--- ConsoleTool/Program.cs
+++ ConsoleTool/Program.cs
@@ -13,14 +13,23 @@ namespace SerialportDataAnalyzer
 		static void Main(string[] args)
 		{
 			Console.Title = "串口数据解析器控制台版";
-
-			Console.WriteLine("解析器已启动，请输入完整发送文件名：");
 			string sendFilePath;
-			sendFilePath = Console.ReadLine();
-
-			Console.WriteLine("解析器已启动，请输入完整接收文件名：");
 			string receiveFilePath;
-			receiveFilePath = Console.ReadLine();
+
+			if(args.Length != 2)
+			{
+				Console.WriteLine("解析器已启动，请输入完整发送文件名：");
+				sendFilePath = Console.ReadLine();
+
+				Console.WriteLine("解析器已启动，请输入完整接收文件名：");
+				receiveFilePath = Console.ReadLine();
+			}
+			else
+			{
+				sendFilePath = args[0];
+				receiveFilePath = args[0];
+			}
+			
 
 			if ((new FileInfo(sendFilePath)).Exists && (new FileInfo(receiveFilePath)).Exists)
 			{
@@ -29,9 +38,7 @@ namespace SerialportDataAnalyzer
 					List<string> errorLog = new List<string>();
 					MainAnalyzer analyzer = new MainAnalyzer(sendFilePath,receiveFilePath, errorLog);
 					analyzer.Analy();
-					foreach(var str in errorLog)
-						Console.WriteLine(str);
-					StreamWriter errorLogFile = new StreamWriter("errorLog.txt", false);
+					StreamWriter errorLogFile = new StreamWriter("ErrorLog.txt", false);
 					foreach(var str in errorLog)
 						errorLogFile.WriteLine(str);
 				}
@@ -51,7 +58,7 @@ namespace SerialportDataAnalyzer
 				Console.WriteLine("文件不存在!");
 			}
 
-			Console.Read();
+			Console.WriteLine("解析结束，错误报告已经导出至ErrorLog.txt");
 		}
 	}
 }
